a2a9d0b9eaa24c65862eee5f9a570aca5dbd765d
jingjietan <2000tanjingjie@gmail.com>
2025-09-20 20:35:53 +0800

    firebase auto deploy

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
